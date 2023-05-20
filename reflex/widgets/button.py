b243491034882eb79cfd1c53ad14b734d7cd7493
Jakob Pinterits <jakob.pinterits@gmail.com>
2023-05-20 22:01:19 +0200

    simplified button internals

--- reflex/widgets/button.py
+++ reflex/widgets/button.py
@@ -21,18 +21,12 @@ class Button(widget_base.Widget):
 
     def build(self) -> rx.Widget:
         return rx.MouseEventListener(
-            rx.Stack(
-                [
-                    rx.Rectangle(
-                        rx.Color.from_rgb(1, 0, 0.5)
-                        if self._is_pressed
-                        else rx.Color.RED
-                    ),
-                    rx.Margin(
-                        rx.Text(self.text),
-                        margin=0.3,
-                    ),
-                ]
+            rx.Rectangle(
+                rx.Color.from_rgb(1, 0, 0.5) if self._is_pressed else rx.Color.RED,
+                rx.Margin(
+                    rx.Text(self.text),
+                    margin=0.3,
+                ),
             ),
             on_mouse_down=self._on_mouse_down,
             on_mouse_up=self._on_mouse_up,
