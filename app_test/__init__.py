236af67055e0e776e742b522bdd70cce1c35243d
Jakob Pinterits <jakob.pinterits@gmail.com>
2023-11-17 18:02:43 +0100

    beginning of AppLattice

---
+++ app_test/__init__.py
@@ -0,0 +1,20 @@
+from dataclasses import field
+from pathlib import Path
+from typing import *  # type: ignore
+
+import rio
+import rio.debug
+
+theme = rio.Theme.from_color()
+
+
+rio_app = rio.App(
+    name="Rio Showcase",
+    build=rio.AppLattice,
+    default_attachments=[
+        theme,
+    ],
+)
+
+
+fastapi_app = rio_app.as_fastapi()
