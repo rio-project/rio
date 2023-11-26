5ce8bc4f7e6fb3864cff6ac72841833d7064ae3a
Jakob Pinterits <jakob.pinterits@gmail.com>
2023-11-26 15:54:26 +0100

    improved value-passing howto, fixed cmdline escaping

--- rio/snippets/snippet-files/howto-passing-values/child_to_parent.py
+++ rio/snippets/snippet-files/howto-passing-values/child_to_parent.py
@@ -3,9 +3,22 @@ import rio
 
 # <code>
 class CustomComponent(rio.Component):
+    some_value: str
+
+    def _on_button_press(self) -> None:
+        print(
+            self.some_value
+        )  # Just read the local value. It will always be up-to-date
+
     def build(self) -> rio.Component:
-        return rio.TextInput(
-            text="Hello world!",
+        return rio.Column(
+            rio.TextInput(
+                text=CustomComponent.some_value,  # Using the class property creates a state binding
+            ),
+            rio.Button(
+                "Click me!",
+                on_press=self._on_button_press,
+            ),
         )
 
 
