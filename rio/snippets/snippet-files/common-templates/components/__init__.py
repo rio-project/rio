88d4ad3dce8597173dcd802dec7ce2dc6b50eb87
Jakob Pinterits <jakob.pinterits@gmail.com>
2023-11-28 10:14:59 +0100

    added placeholder sample components, general project setup tweaks

---
+++ rio/snippets/snippet-files/common-templates/components/__init__.py
@@ -0,0 +1,5 @@
+# This file should never be used for anything. It exists, so that VSCode
+# recognizes this folder as a Python package. This in turn allows components and
+# pages to reference each other, thus enabling refactoring.
+
+from .sample_component import *
