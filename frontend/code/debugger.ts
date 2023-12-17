f5cb4abc6410966a47b1a19dc3deb61cdc7bc15c
Jakob Pinterits <jakob.pinterits@gmail.com>
2023-12-17 10:49:03 +0100

    debugger refactor & prettier navigation

--- frontend/code/debugger.ts
+++ frontend/code/debugger.ts
@@ -1,19 +1,21 @@
 import { MDCRipple } from '@material/ripple';
-import { getInstanceByComponentId } from './componentManagement';
-import { ComponentBase } from './components/componentBase';
-import { FundamentalRootComponent } from './components/fundamental_root_component';
-import { PlaceholderComponent } from './components/placeholder';
-import { textStyleToCss } from './cssUtils';
 import { applyIcon } from './designApplication';
+import { DebuggerTree as DebuggerTreeDriver } from './debuggerTree';
 
-class Debugger {
+export class Debugger {
     rootElement: HTMLElement;
     contentContainer: HTMLElement;
+
+    // Visually highlights the currently active navigation button
+    marker: HTMLElement;
 
     // A string representing which page is currently being displayed in the
     // content container. `null` means that no page is being displayed and the
     // container is hidden.
     displayedPageName: string | null = null;
+
+    // The class managing the current page
+    currentDriver: DebuggerTreeDriver | null = null;
 
     constructor() {
         // Spawn the debugger's HTML
@@ -26,6 +28,7 @@ class Debugger {
         this.rootElement.innerHTML = `
             <div class="rio-debugger-content"></div>
             <div class="rio-debugger-navigation">
+                <div class="rio-debugger-navigation-marker"></div>
                 <div style="flex-grow: 1;"></div>
                 <a href="https://rio.dev" target="_blank" class="rio-debugger-navigation-rio-logo">
                     <img src="/rio/asset/rio-logo.png">
@@ -39,11 +42,59 @@ class Debugger {
             '.rio-debugger-content'
         ) as HTMLElement;
 
+        this.marker = this.rootElement.querySelector(
+            '.rio-debugger-navigation-marker'
+        ) as HTMLElement;
+
         // Initialize the buttons, in reverse order
         this.makeNavButton('AI', 'chat-bubble:fill', 'aiChat');
         this.makeNavButton('Docs', 'library-books:fill', 'docs');
         this.makeNavButton('Stats', 'monitor-heart:fill', 'admin');
         this.makeNavButton('Tree', 'view-quilt:fill', 'componentTree');
+    }
+
+    /// Moves the marker to the given button
+    _updateNavigationMarker(
+        oldButton: HTMLElement | null,
+        newButton: HTMLElement | null
+    ) {
+        console.log('MARKER: ', this.marker);
+
+        // If the button should be hidden, just do that
+        if (newButton === null) {
+            console.log('MARKER: hiding');
+            this.marker.style.opacity = '0';
+            return;
+        }
+
+        // Where should the marker be at?
+        let targetRect = newButton.getBoundingClientRect();
+        console.log('MARKER: target', targetRect);
+
+        // If the button was previously hidden teleport it to the correct
+        // location, then fade it in
+        if (oldButton === null) {
+            console.log('MARKER: teleporting');
+            // Disable CSS transitions
+            this.marker.style.transition = 'none';
+            this.marker.offsetHeight;
+
+            // Teleport
+            this.marker.style.top = `${targetRect.top}px`;
+            this.marker.style.height = `${targetRect.height}px`;
+
+            // Re-enable CSS transitions
+            this.marker.style.removeProperty('transition');
+
+            // Fade in
+            this.marker.style.removeProperty('opacity');
+            return;
+        }
+
+        // Otherwise move it there
+        console.log('MARKER: moving');
+        this.marker.style.top = `${targetRect.top}px`;
+        this.marker.style.height = `${targetRect.height}px`;
     }
 
     /// Display the given page in the content container, replacing the current
@@ -54,20 +105,27 @@ class Debugger {
             return;
         }
 
-        // Clean up
+        // Remove the old page
         if (this.displayedPageName !== null) {
-            // Unselect the old button
-            let oldButton = this.rootElement.querySelector(
-                '.rio-debugger-navigation-button-selected'
-            ) as HTMLElement;
-
-            oldButton.classList.remove(
-                'rio-debugger-navigation-button-selected'
-            );
-
-            // Remove the current page
             this.contentContainer.innerHTML = '';
         }
+
+        // Move the marker
+        let oldButton =
+            this.displayedPageName === null
+                ? null
+                : (this.rootElement.querySelector(
+                      `#rio-debugger-navigation-button-${this.displayedPageName}`
+                  ) as HTMLElement);
+
+        let newButton =
+            pageName === null
+                ? null
+                : (this.rootElement.querySelector(
+                      `#rio-debugger-navigation-button-${pageName}`
+                  ) as HTMLElement);
+
+        this._updateNavigationMarker(oldButton, newButton);
 
         // Update the state
         this.displayedPageName = pageName;
@@ -77,16 +135,10 @@ class Debugger {
             return;
         }
 
-        // Select the new button
-        let newButton = this.rootElement.querySelector(
-            `#rio-debugger-navigation-button-${pageName}`
-        ) as HTMLElement;
-
-        newButton.classList.add('rio-debugger-navigation-button-selected');
-
         // Add the new page
         if (pageName === 'componentTree') {
-            this.contentContainer.appendChild(this.makeTreeView());
+            this.currentDriver = new DebuggerTreeDriver(this);
+            this.contentContainer.appendChild(this.currentDriver.rootElement);
         } else {
             console.error(`Unknown debugger page: ${pageName}`);
         }
@@ -122,236 +174,6 @@ class Debugger {
             event?.stopPropagation();
             this.navigateTo(navTarget);
         });
-    }
-
-    highlightTreeItemAndParents(instance: ComponentBase) {
-        // Unhighlight all previously highlighted items
-        for (let element of Array.from(
-            document.querySelectorAll(
-                '.rio-debugger-component-tree-item-header-weakly-selected, .rio-debugger-component-tree-item-header-strongly-selected'
-            )
-        )) {
-            element.classList.remove(
-                'rio-debugger-component-tree-item-header-weakly-selected',
-                'rio-debugger-component-tree-item-header-strongly-selected'
-            );
-        }
-
-        // Find all tree items
-        let treeItems: HTMLElement[] = [];
-
-        let cur: HTMLElement | null = document.getElementById(
-            `rio-debugger-component-tree-item-${instance.elementId}`
-        ) as HTMLElement;
-
-        console.log(
-            `rio-debugger-component-tree-item-${instance.elementId}`,
-            cur,
-            cur.classList.contains('rio-debugger-component-tree-item')
-        );
-
-        while (
-            cur !== null &&
-            cur.classList.contains('rio-debugger-component-tree-item')
-        ) {
-            treeItems.push(cur.firstElementChild as HTMLElement);
-            cur = cur.parentElement!.parentElement;
-        }
-
-        // Strongly select the leafmost item
-        treeItems[0].classList.add(
-            'rio-debugger-component-tree-item-header-strongly-selected'
-        );
-
-        // Weakly select the rest
-        for (let i = 1; i < treeItems.length; i++) {
-            treeItems[i].classList.add(
-                'rio-debugger-component-tree-item-header-weakly-selected'
-            );
-        }
-    }
-
-    makeTreeNode(
-        parentElement: HTMLElement,
-        instance: ComponentBase,
-        level: number
-    ) {
-        // Create the element for this item
-        let element = document.createElement('div');
-        element.id = `rio-debugger-component-tree-item-${instance.elementId}`;
-        element.classList.add('rio-debugger-component-tree-item');
-        parentElement.appendChild(element);
-
-        // Create the header
-        let header = document.createElement('div');
-        header.classList.add('rio-debugger-component-tree-item-header');
-        header.textContent = instance.state._python_type_;
-        element.appendChild(header);
-
-        // Add the children
-        let children = instance.getDirectChildren();
-        let childElement = document.createElement('div');
-        childElement.classList.add('rio-debugger-component-tree-item-children');
-        childElement.style.marginLeft = '0.7rem';
-        childElement.style.display = 'none';
-        element.appendChild(childElement);
-
-        for (let childInfo of children) {
-            this.makeTreeNode(childElement, childInfo, level + 1);
-        }
-
-        // Add icons to give additional information
-        let icons: string[] = [];
-
-        // Icon: Container
-        if (children.length <= 1) {
-        } else if (children.length > 9) {
-            icons.push('filter-9-plus');
-        } else {
-            icons.push(`filter-${children.length}`);
-        }
-
-        // Icon: Key
-        if (instance.state._key_ !== null) {
-            icons.push('key');
-        }
-
-        let spacer = document.createElement('div');
-        spacer.style.flexGrow = '1';
-        header.appendChild(spacer);
-
-        for (let icon of icons) {
-            let iconElement = document.createElement('div');
-            header.appendChild(iconElement);
-            applyIcon(iconElement, icon, 'currentColor');
-        }
-
-        // Toggle the children when the element is clicked
-        header.addEventListener('click', (event) => {
-            childElement.style.display =
-                childElement.style.display === 'none' ? 'flex' : 'none';
-            event.stopPropagation();
-        });
-
-        // Allow the user to select an item
-        header.addEventListener('click', (event) => {
-            // Highlight the tree item
-            this.highlightTreeItemAndParents(instance);
-
-            // Scroll to the element
-            let componentElement = instance.element();
-            componentElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center',
-                inline: 'center',
-            });
-
-            event.stopPropagation();
-        });
-
-        // Highlight the actual component when the element is hovered
-        header.addEventListener('mouseover', (event) => {
-            // Get the element for this instance
-            let componentElement = instance.element();
-            let rect = componentElement.getBoundingClientRect();
-
-            // Highlight the component
-            let highlighter = document.createElement('div');
-            highlighter.classList.add('rio-debugger-component-highlighter');
-            highlighter.style.top = `${rect.top}px`;
-            highlighter.style.left = `${rect.left}px`;
-            highlighter.style.width = `${rect.width}px`;
-            highlighter.style.height = `${rect.height}px`;
-            document.body.appendChild(highlighter);
-
-            event.stopPropagation();
-        });
-
-        // Remove any highlighters when the element is unhovered
-        element.addEventListener('mouseout', (event) => {
-            for (let highlighter of document.getElementsByClassName(
-                'rio-debugger-component-highlighter'
-            )) {
-                highlighter.remove();
-            }
-
-            event.stopPropagation();
-        });
-    }
-
-    makeTreeView(): HTMLElement {
-        let element = document.createElement('div');
-        element.classList.add('rio-debugger-tree-view');
-
-        element.innerHTML = `
-            <div class="rio-debugger-header">
-                Component Tree
-            </div>
-
-            <div class="rio-debugger-tree-component-tree">
-                <div></div>
-            </div>
-
-            <div class="rio-debugger-tree-component-details-heading">Component Details</div>
-            <div class="rio-debugger-tree-component-details"></div>
-            <a class="rio-debugger-tree-component-docs-link">
-                <div></div> View Documentation
-            </a>
-        `;
-
-        // Header
-        let header = element.querySelector(
-            '.rio-debugger-header'
-        ) as HTMLElement;
-        Object.assign(header.style, textStyleToCss('heading2'));
-
-        // The tree itself
-        //
-        // This has a helper element to make the tree scrollable
-        let treeOuter = element.querySelector(
-            '.rio-debugger-tree-component-tree'
-        ) as HTMLElement;
-
-        let treeInner = treeOuter.firstElementChild as HTMLElement;
-
-        // Add a section to display details about the selected component
-        let detailsHeading = element.querySelector(
-            '.rio-debugger-tree-component-details-heading'
-        ) as HTMLElement;
-        Object.assign(detailsHeading.style, textStyleToCss('heading3'));
-
-        let details = element.querySelector(
-            '.rio-debugger-tree-component-details'
-        ) as HTMLElement;
-        details.textContent = 'TODO: Details';
-
-        // Documentation link
-        let docsLink = element.querySelector(
-            '.rio-debugger-tree-component-docs-link'
-        ) as HTMLElement;
-
-        applyIcon(
-            docsLink.firstElementChild as HTMLElement,
-            'library-books',
-            'currentColor'
-        );
-
-        // Get the rootmost element. However, take care to skip the elements
-        // internal to Rio.
-        let highLevelRootComponent = getInstanceByComponentId(
-            0
-        ) as PlaceholderComponent; // TODO: Use a better method once the layouting rework is merged
-        let fundamentalRootComponent = getInstanceByComponentId(
-            highLevelRootComponent.state._child_
-        ) as FundamentalRootComponent;
-        let userRootComponent = getInstanceByComponentId(
-            fundamentalRootComponent.state.child
-        );
-
-        // Build the tree
-        this.makeTreeNode(treeInner, userRootComponent, 0);
-
-        return element;
     }
 }
 
--- frontend/code/easeFunctions.ts
+++ frontend/code/easeFunctions.ts
@@ -1,20 +1,51 @@
-export function easeIn(a: number): number {
-    return Math.pow(a, 2);
+export function easeIn(t: number): number {
+    return Math.pow(t, 3);
 }
 
-export function easeOut(a: number): number {
-    return 1 - Math.pow(1 - a, 2);
+export function easeOut(t: number): number {
+    return 1 - easeIn(1 - t);
 }
 
-export function easeInOut(a: number): number {
-    // Map to the sine function's domain
-    a = (a - 0.5) * Math.PI;
+export function easeInOut(t: number): number {
+    return t < 0.5 ? easeIn(t * 2) / 2 : easeOut(t * 2 - 1) / 2 + 0.5;
+}
 
-    // Apply the sine function
-    a = Math.sin(a);
+function cubicBezier(
+    t: number,
+    p2: [number, number],
+    p3: [number, number]
+): number {
+    // FIXME: I have about zero confidence that this function is correct. It is
+    //        not at all tested.The results look okay though
 
-    // Map back to [0, 1]
-    a = (a + 1) / 2;
+    // Define all control points
+    const p1 = [0, 0];
+    const p4 = [1, 1];
 
-    return a;
+    // Project the given t value onto the curve
+    let tCurve = t;
+    for (let i = 0; i < 5; i++) {
+        let current =
+            Math.pow(1 - tCurve, 3) * p1[0] +
+            3 * Math.pow(1 - tCurve, 2) * tCurve * p2[0] +
+            3 * (1 - tCurve) * Math.pow(tCurve, 2) * p3[0] +
+            Math.pow(tCurve, 3) * p4[0];
+        let derivative =
+            3 * Math.pow(1 - tCurve, 2) * p2[0] +
+            6 * (1 - tCurve) * tCurve * (p3[0] - p2[0]) +
+            3 * Math.pow(tCurve, 2) * (p4[0] - p3[0]);
+        tCurve -= (current - t) / derivative;
+    }
+
+    // Calculate the y value corresponding to t' on the curve
+    return (
+        Math.pow(1 - tCurve, 3) * p1[1] +
+        3 * Math.pow(1 - tCurve, 2) * tCurve * p2[1] +
+        3 * (1 - tCurve) * Math.pow(tCurve, 2) * p3[1] +
+        Math.pow(tCurve, 3) * p4[1]
+    );
+}
+
+export function overshoot(t: number): number {
+    return cubicBezier(t, [0.5, 0.5], [0.2, 1.14]);
 }
