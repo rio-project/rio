49b64d40af6b331c03d7d529f00259c63aadba41
Jakob Pinterits <jakob.pinterits@gmail.com>
2023-12-24 23:25:07 +0100

    debugger: show details about the selected component

--- rio/debug/client_side_debugger/tree_page.py
+++ rio/debug/client_side_debugger/tree_page.py
@@ -1,13 +1,20 @@
+from typing import *  # type: ignore
+
 import rio
 
 from . import component_details
 
 
 class TreePage(rio.Component):
+    _selected_component_id: Optional[int] = None
+
+    def _on_select_component(self, component_id: int) -> None:
+        self._selected_component_id = component_id
+
     def build(self) -> rio.Component:
         margin = 1
 
-        return rio.Column(
+        children = [
             rio.Text(
                 "Component Tree",
                 style="heading2",
@@ -19,8 +26,16 @@ class TreePage(rio.Component):
                 width=22,
                 height="grow",
                 margin=margin,
+                on_select_component=self._on_select_component,
             ),
-            component_details.ComponentDetails(
-                margin=margin,
-            ),
-        )
+        ]
+
+        if self._selected_component_id is not None:
+            children.append(
+                component_details.ComponentDetails(
+                    component_id=self._selected_component_id,
+                    margin=margin,
+                )
+            )
+
+        return rio.Column(*children)
