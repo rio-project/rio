66fe24c60569c237b4e7a492b835eef1a296ceed
Jakob Pinterits <jakob.pinterits@gmail.com>
2024-02-01 22:44:17 +0100

    bugfix: reuse ports in `port_is_free`

--- rio/common.py
+++ rio/common.py
@@ -235,6 +235,8 @@ def choose_free_port(host: str) -> int:
 
 def port_is_free(host: str, port: int) -> bool:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
         try:
             sock.bind((host, port))
             return True
