ba92572e6753c7a43219f34b636f4b4c2b42be67
Jakob Pinterits <jakob.pinterits@gmail.com>
2024-02-23 17:47:00 +0100

    added the WIP dashboard to the repository

---
+++ dashboard/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import *  # type: ignore
+
+import rio
+
+from . import components as comps
+from . import pages
+
+app = rio.App(
+    name="Rio",
+    icon=rio.common.RIO_LOGO_ASSET_PATH,
+    pages=[
+        rio.Page(
+            page_url="",
+            build=pages.Dashboard,
+        ),
+    ],
+    theme=rio.Theme.from_color(
+        primary_color=rio.Color.from_hex("a200f0"),
+    ),
+)
